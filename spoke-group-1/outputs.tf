71edb24ee128c444a92fffe0afb7b6fee28794ba
Carlos Santana <csantana23@gmail.com>
2023-04-06 11:07:12 -0400

    lint
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- spoke-group-1/outputs.tf
+++ spoke-group-1/outputs.tf
@@ -1,6 +1,6 @@
 output "spoke_kubeconfig" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = [
+  value = [
     module.spoke_cluster_1.configure_kubectl,
     module.spoke_cluster_2.configure_kubectl,
     module.spoke_cluster_10.configure_kubectl
