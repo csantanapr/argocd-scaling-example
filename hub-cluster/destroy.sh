4d8374999431bed1182d0d0e5128e84f4dc19d61
Carlos Santana <csantana23@gmail.com>
2023-05-03 10:37:53 -0400

    update destroy for hub-cluster
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hub-cluster/destroy.sh
+++ hub-cluster/destroy.sh
@@ -7,8 +7,10 @@ kubectl_login=$(terraform output -raw configure_kubectl)
 $kubectl_login
 kubectl delete ing -A --all
 
-terraform state rm grafana_dashboard.argocd
-terraform state rm grafana_data_source.prometheus
+terraform destroy -target="grafana_dashboard.argocd" -auto-approve
+terraform destroy -target="grafana_data_source.prometheus" -auto-approve
+terraform destroy -target="helm_release.keycloak" -auto-approve
+
 terraform destroy -target="module.eks_blueprints_argocd_workloads" -auto-approve
 terraform destroy -target="module.eks_blueprints_argocd_addons" -auto-approve
 terraform destroy -target="module.eks_blueprints_kubernetes_addons" -auto-approve
